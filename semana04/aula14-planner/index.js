b4428336dbf8b66c188e7fedd9c3c25879eff7ad
B-Mugnol <bruno_mugnol@hotmail.com>
2020-10-06 16:19:02 -0300

    Aula 14 - planner (desafio incompleto)

---
+++ semana04/aula14-planner/index.js
@@ -0,0 +1,49 @@
+const dayArray = ["domingo", "segunda", "terca", "quarta", "quinta", "sexta", "sabado"]
+let taskCounter = 0
+
+const newTask = () =>{
+    const task = document.getElementById("tarefa")
+    if (task.value === ""){
+        alert ("Insira uma tarefa válida.")
+        return
+    }
+    let taskAdded = task.value
+    task.value = ""
+    return taskAdded
+}
+
+const valueDayOfWeek = () =>{
+    const weekday = document.getElementById("dias-semana")
+    let dayAdded = weekday.value
+    return dayAdded
+}
+
+const addUnorderedList = (day) =>{
+    const container = document.getElementById(`${day}`)
+
+    if (container.innerHTML === ""){
+        container.innerHTML = `<ul id=${day + "-list"}></ul>`
+    }
+}
+
+const addListItem = () =>{
+    addUnorderedList(valueDayOfWeek())
+    const unorderedList = document.getElementById(`${valueDayOfWeek() + "-list"}`)
+    taskCounter++
+    unorderedList.innerHTML += `<li id=${"taskNumber" + taskCounter} onclick="lineThroughText()">${newTask()}</li>`
+}
+
+const eraseTasks = () =>{
+    if(confirm("Você tem certeza? Essa ação é irreversível")){
+        let dayDiv
+        for (let i=0; i < dayArray.length; i++){
+            dayDiv = document.getElementById(`${dayArray[i]}`)
+            dayDiv.innerHTML = ""
+        }
+    }
+}
+
+const lineThroughText = () =>{
+    const target = document.
+    target.style.property = "line-through"
+}
