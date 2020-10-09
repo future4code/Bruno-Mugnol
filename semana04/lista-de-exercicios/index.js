a139e33baebab7a205bc5afe32fa74de5e0e4c83
B-Mugnol <bruno_mugnol@hotmail.com>
2020-10-09 11:17:45 -0300

    Exercício 1 de interpretação de código

---
+++ semana04/lista-de-exercicios/index.js
@@ -0,0 +1,8 @@
+// Exercícios de interpretação de código
+// Exercício 1:
+/* Cria uma função de 1 parâmetro que pede ao usuário a inserção de um valor, o transforma em tipo Number, e retorna "R$" seguido do parâmetro multiplicado pelo valor inserido.
+Cria uma variável que invoca a função, substituindo o parâmetro pelo valor 100. A função retornará: "R$"  + 100*cotacao, sendo que cotacao é um número que depende da inserção do usuário.
+Imprime essa variável no console. */
+
+// Exercício 2:
+/* 
