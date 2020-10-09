cc7299c76e9feae321f79637de51402b4d5a3006
B-Mugnol <bruno_mugnol@hotmail.com>
2020-10-09 12:11:54 -0300

    Exercício 2 de interpretação de código

--- semana04/lista-de-exercicios/index.js
+++ semana04/lista-de-exercicios/index.js
@@ -4,5 +4,23 @@
 Cria uma variável que invoca a função, substituindo o parâmetro pelo valor 100. A função retornará: "R$"  + 100*cotacao, sendo que cotacao é um número que depende da inserção do usuário.
 Imprime essa variável no console. */
 
+
+
 // Exercício 2:
-/* 
+/* Cria uma função que pede dois parâmetros: tipoDeInvestimento e valor. tipoDeInvestimento é do tipo string, e deve assumir um dos valores: "Poupança", "Renda Fixa", "CDB" ou "Ações", senão retorna um alerta ao usuário dizendo "TIPO DE INVESTIMENTO INFORMADO INCORRETO!".
+Dentro da função cria-se uma variável valorAposInvestimento, cujo valor altera dependendo do tipoDeInvestimento, pois este é comparado em um switch case e cada case é para um dos valores possíveis mencionados anteriormente.
+A função retorna valorAposInvestimento.
+
+novoMontante invoca a função investeDinheiro com os parâmetros "Ações" e 150, ou seja, onde havia tipoDeInvestimento na função será rodado com "Ações", e onde havia valor, com 150.
+
+segundoMontante invoca a função investeDinheiro com os parâmetros "Tesouro Direto" e 200, ou seja, onde havia tipoDeInvestimento na função será rodado com "Tesouro Direto", e onde havia valor, com 200. Como "Tesouro Direto" não é um tipo válido, a função, ao ser rodada, emitirá o alerta ao usuário, e não irá alterar o valorAposInvestimento.
+
+Como "Ações" é um tipo de investimento válido, console.log(novoMontante) retornará valorAposInvestimento, com seu novo valor: 150 * 1.1, ou seja, aparecerá no console, em azul (pois é tipo number):
+
+165
+
+Já em segundoMontante: a função retorna valorAposInvestimento após ter dado o alerta, mas como foi definido vazio, valorAposInvestimento = undefined. Assim, aparecerá no console:
+
+undefined
+
+*/
