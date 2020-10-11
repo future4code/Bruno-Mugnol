1e00f8189f779750b521204643252a321e8f7068
B-Mugnol <bruno_mugnol@hotmail.com>
2020-10-11 11:16:51 -0300

    Exercício 3 de funções de array

--- semana04/lista-de-exercicios/index.js
+++ semana04/lista-de-exercicios/index.js
@@ -333,3 +333,31 @@ const arrayDeStrings = (array) => {
     }
     return arrayDeStrings
 }
+
+
+
+// Exercício 3
+const pessoas = [
+	{ nome: "Paula", idade: 12, altura: 1.8},
+	{ nome: "João", idade: 20, altura: 1.3},
+	{ nome: "Pedro", idade: 15, altura: 1.9},
+	{ nome: "Luciano", idade: 22, altura: 1.8},
+	{ nome: "Artur", idade: 10, altura: 1.2},
+	{ nome: "Soter", idade: 70, altura: 1.9}
+]
+
+// 3.a)
+const permitirPessoas = (pessoas) => {
+    let pessoasPermitidas = pessoas.filter((pessoa) => {
+        return (pessoa.altura >= 1.5) && (pessoa.idade >= 14) && (pessoa.idade < 60)
+    })
+    return pessoasPermitidas
+}
+
+// 3.b)
+const impedirPessoas = (pessoas) => {
+    let pessoasImpedidas = pessoas.filter((pessoa) => {
+        return (pessoa.altura < 1.5) || (pessoa.idade < 14) || (pessoa.idade >= 60)
+    })
+    return pessoasImpedidas
+}
