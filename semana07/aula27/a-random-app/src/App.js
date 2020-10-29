7db74c8fe71e1e2721b5fc97ba34847fa6446565
B-Mugnol <bruno_mugnol@hotmail.com>
2020-10-29 17:01:22 -0300

    Início projeto

---
+++ semana07/aula27/a-random-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import axios from 'axios'
+import styled from 'styled-components';
+
+class App extends React.Component {
+
+  render() {
+
+    return (
+      <div>
+        oi
+      </div>
+    )
+  }
+}
+
+export default App;
