88cd11d8a90dc0dcbfbb08cf88f86b8bc337840e
B-Mugnol <bruno_mugnol@hotmail.com>
2020-11-16 19:47:07 -0300

    Criação das páginas e arquivos

---
+++ semana10/labex/src/pages/ApplicationPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ApplicationPage = () => {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default ApplicationPage
---
+++ semana10/labex/src/pages/TripDetailPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const TripDetailPage = () => {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default TripDetailPage
