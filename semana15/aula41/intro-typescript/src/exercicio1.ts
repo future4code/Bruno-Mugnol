5dcf42bdb49d42bf70b8ed32883288cdb666a0ba
B-Mugnol <bruno_mugnol@hotmail.com>
2021-01-05 11:17:26 -0300

    Exercício 2

---
+++ semana15/aula41/intro-typescript/src/exercicio1.ts
@@ -0,0 +1,62 @@
+// a)
+// const myString: string = 420
+// O editor aponta um erro. A transpilação não ocorre.
+
+// b)
+const myNumber: number | string = 13
+
+// c)
+const person1: {name: string, age: number, favColour: string} = {
+    name: "Bob",
+    age: 99,
+    favColour: "verde"
+}
+
+// d)
+type person = {
+    name: string,
+    age: number,
+    favColour: string
+}
+
+const person2: person = {
+    name: "Billy",
+    age: 13,
+    favColour: "branco"
+}
+
+const person3: person = {
+    name: "Joaquim",
+    age: 10,
+    favColour: "cinza"
+}
+
+const person4: person = {
+    name: "Matilde",
+    age: 7,
+    favColour: "laranja"
+}
+
+// e)
+enum rainbowColors {
+    RED = "vermelho",
+    ORANGE = "laranja",
+    YELLOW = "amarelo",
+    GREEN = "verde",
+    CYAN = "azul claro",
+    BLUE = "azul",
+    VIOLET = "roxo"
+}
+
+type basicHuman = {    
+    name: string,
+    age: number,
+    favColour: rainbowColors
+}
+
+const person5: basicHuman = {
+    name: "Squirrel",
+    age: 3,
+    favColour: rainbowColors.VIOLET
+}
+console.log(person5)
