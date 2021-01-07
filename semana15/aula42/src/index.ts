b5d3547804d077d5825fb249c4bde046820b7ab8
B-Mugnol <bruno_mugnol@hotmail.com>
2021-01-07 11:05:17 -0300

    Endpoint 4 completo

--- semana15/aula42/src/index.ts
+++ semana15/aula42/src/index.ts
@@ -32,6 +32,7 @@ app.get('/countries/all', (req: Request, res: Response) => {
 
 app.get('/countries/search', (req: Request, res: Response) => {
     let errorCode: number = 400
+    let errorMessage: string = ""
 
     const validQueries = ['name', 'capital', 'continent']
 
@@ -41,6 +42,7 @@ app.get('/countries/search', (req: Request, res: Response) => {
         const isValidQuery = (queries: string[]): boolean => {
             for (let query of queries) {
                 if (!validQueries.includes(query)) {
+                    errorMessage = "Invalid query parameters."
                     return false
                 }
             }
@@ -50,25 +52,92 @@ app.get('/countries/search', (req: Request, res: Response) => {
 
         if (!usedQuery.length || !isValidQuery(usedQuery)) {
             errorCode = 422
-            throw new Error()
+            throw new Error(errorMessage || "Invalid queries.")
         }
 
         let result: country[] = countries
 
         for (let searchParam in req.query) {
+            const reqQuery: string = req.query[searchParam] as string
+
             result = result.filter((aCountry: any) => {
-                return aCountry[searchParam].includes(req.query[searchParam])
+                return aCountry[searchParam].toLowerCase().includes(reqQuery.toLowerCase())
             })
         }
-        
+
         if (!result.length) {
             errorCode = 404
-            throw new Error()
+            throw new Error("Country not found.")
         }
 
         res.status(200).send(result)
+
+    } catch (error) {
+        console.log(error)
+        res.status(errorCode).end()
     }
-    catch (error) {
+})
+
+app.put('/countries/edit/:id', (req: Request, res: Response) => {
+    let errorCode: number = 400
+    let errorMessage: string = ""
+
+    const editId: number = Number(req.params.id)
+
+    type bodyType = {
+        name: string,
+        capital: string
+    }
+
+    const validBody: string[] = ['name', 'capital']
+
+    try {
+        if (isNaN(editId)) {
+            errorCode = 422
+            throw new Error("Invalid ID.")
+        }
+
+        const isValidBody = (body: bodyType): boolean => {
+            for (let key in req.body) {
+                if (!validBody.includes(key)) {
+                    errorCode = 422
+                    errorMessage = "Invalid body key."
+                    return false
+                }
+
+                if (!key) {
+                    errorCode = 401
+                    errorMessage = "Found an empty required field."
+                    return false
+                }
+            }
+
+            return true
+        }
+
+        if (!isValidBody(req.body)) {
+            throw new Error(errorMessage)
+        }
+
+        const countryIndex: number = countries.findIndex((country) => {
+            return country.id === editId
+        })
+
+        if (countryIndex === -1) {
+            errorCode = 404
+            errorMessage = "Country ID not found."
+            throw new Error(errorMessage)
+        }
+
+        countries[countryIndex] = {
+            ...countries[countryIndex],
+            name: req.body.name,
+            capital: req.body.capital
+        }
+
+        res.status(200).send("Country edited sucessfully.")
+
+    } catch (error) {
         console.log(error)
         res.status(errorCode).end()
     }
@@ -81,7 +150,7 @@ app.get('/countries/:id', (req: Request, res: Response) => {
     try {
         if (isNaN(searchedId)) {
             errorCode = 422
-            throw new Error()
+            throw new Error("Invalid ID.")
         }
 
         const searchedCountry: country | undefined = countries.find((country: country): boolean => {
@@ -90,13 +159,12 @@ app.get('/countries/:id', (req: Request, res: Response) => {
 
         if (!searchedCountry) {
             errorCode = 404
-            throw new Error()
+            throw new Error("Country not found.")
         }
 
         res.status(200).send(searchedCountry)
 
-    }
-    catch (error) {
+    } catch (error) {
         console.log(error)
         res.status(errorCode).end()
     }
