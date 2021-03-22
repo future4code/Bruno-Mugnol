e9ef5eb1b3e0447f74b6dcbdd81692b7c449aa7e
B-Mugnol <bruno_mugnol@hotmail.com>
2021-03-22 17:11:22 -0300

    Refactored findMissingNumber and its tests to allow quantity of missing numbers

--- logic-exercises/src/findMissingNumber.ts
+++ logic-exercises/src/findMissingNumber.ts
@@ -1,15 +1,16 @@
-export const findMissingNumber = (array: number[]): number | void => {
+export const findMissingNumber = (array: number[], lastNumber: number = 100): number[] => {
     const sortedArray: Array<number | undefined> = []
+    const missingNumbers: number[] = []
 
     for (const num of array) {
         sortedArray[num - 1] = num
     }
 
-    for (let i = 0; i < sortedArray.length; i++) {
-        if (sortedArray[i] === undefined) return i + 1
+    for (let i = 0; i < lastNumber; i++) {
+        if (sortedArray[i] === undefined) {
+            missingNumbers.push(i + 1)
+        }
     }
 
-    if (sortedArray[array.length - 1] !== 100) {
-        return 100
-    } else return
+    return missingNumbers
 }
--- logic-exercises/tests/findMissingNumber.test.ts
+++ logic-exercises/tests/findMissingNumber.test.ts
@@ -1,7 +1,7 @@
 import { findMissingNumber } from '../src/findMissingNumber'
 
 describe("Testing findMissingNumber", () => {
-    it("Should return 33", () => {
+    it("Should return [33]", () => {
         const numberArray: number[] = []
 
         for (let i = 1; i <= 100; i++) numberArray.push(i)
@@ -9,23 +9,24 @@ describe("Testing findMissingNumber", () => {
 
         const result = findMissingNumber(numberArray)
 
-        expect(result).toBe(33)
+        expect(result).toEqual([33])
     })
 
 
-    it("Should return 33, but for unordered array", () => {
+    it("Should return [33, 57], even for unordered array", () => {
         const numberArray: number[] = []
 
         for (let i = 100; i >= 1; i--) numberArray.push(i)
         numberArray.splice(67, 1)
+        numberArray.splice(43, 1)
 
         const result = findMissingNumber(numberArray)
 
-        expect(result).toBe(33)
+        expect(result).toEqual([33, 57])
     })
 
 
-    it("Should return 100", () => {
+    it("Should return [100]", () => {
         const numberArray: number[] = []
 
         for (let i = 1; i <= 100; i++) numberArray.push(i)
@@ -33,17 +34,30 @@ describe("Testing findMissingNumber", () => {
 
         const result = findMissingNumber(numberArray)
 
-        expect(result).toBe(100)
+        expect(result).toEqual([100])
     })
 
 
-    it("Should return undefined if no number is missing", () => {
+    it("Should return [1, 200]", () => {
+        const numberArray: number[] = []
+
+        for (let i = 1; i <= 200; i++) numberArray.push(i)
+        numberArray.splice(199, 1)
+        numberArray.splice(0, 1)
+
+        const result = findMissingNumber(numberArray, 200)
+
+        expect(result).toEqual([1, 200])
+    })
+
+
+    it("Should return [] if no number is missing", () => {
         const numberArray: number[] = []
 
         for (let i = 1; i <= 100; i++) numberArray.push(i)
 
         const result = findMissingNumber(numberArray)
 
-        expect(result).toBe(undefined)
+        expect(result.length).toBe(0)
     })
 })
