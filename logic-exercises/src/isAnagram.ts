1ed99be0eb6dbbbb56eea924240436d0ca8fc355
B-Mugnol <bruno_mugnol@hotmail.com>
2021-03-30 09:35:27 -0300

    included length comparison to avoid unnecessary iterations

--- logic-exercises/src/isAnagram.ts
+++ logic-exercises/src/isAnagram.ts
@@ -1,4 +1,6 @@
 export const isAnagram = (str1: string, str2: string, caseSensitive: boolean = true): boolean => {
+    if (str1.length !== str2.length) return false
+
     return caseSensitive ?
         str1
             .split("").sort().join("")
