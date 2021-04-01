8d19cfdb5bdcc60710915690993dee8c8e5e7861
B-Mugnol <bruno_mugnol@hotmail.com>
2021-04-01 10:35:58 -0300

    countNegatives

--- logic-exercises/src/countNegatives.ts
+++ logic-exercises/src/countNegatives.ts
@@ -1,8 +1,21 @@
 type Matrix = number[][]
 
-const returnMatrixSize = (m: Matrix): { rows: number, columns: number } => {
-    return {
-        rows: m.length,
-        columns: m[0].length
+
+const countNegatives = (sortedMatrix: Matrix): number => {
+    const [rows, columns] = [sortedMatrix.length, sortedMatrix[0].length]
+
+    let negativeCount = 0
+    let i = rows - 1
+    let j = 0
+
+    while (j < columns && i >= 0) {
+        if (sortedMatrix[i][j] < 0) {
+            negativeCount += columns - j
+            i--
+        } else {
+            j++
+        }
     }
+
+    return negativeCount
 }
